d72779f56937d04e33e580638a47ff3d07fec109
Jerscape <jeremydutton5@gmail.com>
2023-07-19 14:34:48 -0400

    retrieve json object url

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express();
-
-const port = 8080;
+const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,3 +10,11 @@ const urlDatabase = {
 app.get("/", (req, res) =>{
   res.send("Hello");
 })
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
