9e368f5668bbddc1f7d9af7904ffeabc07941d2d
Jerscape <jeremydutton5@gmail.com>
2023-07-20 15:55:39 -0400

    add urls_show view

--- express_server.js
+++ express_server.js
@@ -6,8 +6,8 @@ app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  "9sm5xK": "http://www.google.com",
+  };
 
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase} 
@@ -26,6 +26,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res)=> {
   res.send("<html><body>Hello <b>World</b></body><html>\n")
 })
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id
+  const templateVars = { id: req.params.id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
