015a49032346d02041fec63be9093b8bf4f70fb1
Jerscape <jeremydutton5@gmail.com>
2023-07-20 20:43:41 -0400

    add form view as well as post route

--- express_server.js
+++ express_server.js
@@ -4,11 +4,19 @@ const PORT = 8080;
 
 app.set("view engine", "ejs")
 
+
+function generateRandomString() {}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   };
 
+//converts request body to a string 
+//it then adds the data to the request object under the key "body"
+app.use(express.urlencoded({ extended: true }));
+
+//get routes
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase} 
   res.render("urls_index", templateVars)
@@ -27,10 +35,20 @@ app.get("/hello", (req, res)=> {
   res.send("<html><body>Hello <b>World</b></body><html>\n")
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id
   const templateVars = { id: req.params.id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
+});
+
+//post routes
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
